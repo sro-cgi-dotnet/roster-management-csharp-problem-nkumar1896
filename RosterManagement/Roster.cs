d10a6b9e3e6f2c7a986364808adf07d7be4a07ab
narendar kumar <nkumar010896@gmail.com>
2018-09-16 01:01:30 +0530

    solved

--- RosterManagement/Roster.cs
+++ RosterManagement/Roster.cs
@@ -20,6 +20,18 @@ namespace RosterManagement
         /// <param name="wave">Refers to the Wave number</param>
         public void Add(string cadet, int wave)
         {
+           //check if the wave already exist or not
+            if(_roster.ContainsKey(wave))
+            {
+
+              _roster[wave].Add(cadet);
+            }
+            else
+            {
+                _roster.Add(wave, new List<string>());
+                _roster[wave].Add(cadet);
+            }
+            
         }
 
         /// <summary>
@@ -29,7 +41,18 @@ namespace RosterManagement
         /// <returns>List of Cadet's Name</returns>
         public List<string> Grade(int wave)
         {
+          //Dictionary<int,List<string>>  rost1 = new Dictionary<int, List<String>>();
             var list = new List<string>();
+          
+           if(_roster.ContainsKey(wave))
+           {
+               foreach(string s in _roster[wave])
+               {
+                   list.Add(s);
+               }
+               list.Sort();
+           }
+            
             return list;
         }
 
@@ -39,7 +62,21 @@ namespace RosterManagement
         /// <returns>List of Cadet's Name</returns>
         public List<string> Roster()
         {
-            var cadets = new List<string>();
+            var cadets=new List<string>();
+            var waves=new List<int>();
+            waves=_roster.Keys.ToList();
+            //sort a list of wave first
+            waves.Sort();
+            foreach(int i in waves)
+            {
+                //sort the names of each sorted wave
+                _roster[i].Sort();
+                foreach(string s in _roster[i])
+                {
+                
+                  cadets.Add(s);
+                }
+            }
             return cadets;
         }
     }
